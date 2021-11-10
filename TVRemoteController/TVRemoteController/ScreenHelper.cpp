b1de203bad68a55c5189d668b5fa1145aa9348e0
Sancho Amaral Simões <ssimoes@ipn.pt>
2021-11-10 01:04:44 +0000

    MAIN: Add screen helper.

---
+++ TVRemoteController/TVRemoteController/ScreenHelper.cpp
@@ -0,0 +1,10 @@
+#include "wtypes.h"
+
+void GetDesktopResolution(int& horizontal, int& vertical)
+{
+	RECT desktop;
+	const HWND hDesktop = GetDesktopWindow();
+	GetWindowRect(hDesktop, &desktop);
+	horizontal = desktop.right;
+	vertical = desktop.bottom;
+}

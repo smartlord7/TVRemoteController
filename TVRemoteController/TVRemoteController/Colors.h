0a5c20a8ee0ebf65f5003357591b2160064346ff
Sancho Amaral Simões <ssimoes@ipn.pt>
2021-11-10 01:05:47 +0000

    MAIN: Add colors header file.

---
+++ TVRemoteController/TVRemoteController/Colors.h
@@ -0,0 +1,13 @@
+#pragma once
+// COLORS_H
+#ifndef COLORS_H
+#define COLORS_H
+
+//#region Colors
+
+#define BLACK 0.0, 0.0, 0.0, 1.0
+
+//#endregion Colors
+
+
+#endif //#endregion Window Parameters
